8a5da288f57e2c2f8b836fee2af35954930e9399
Raziel <razielpltt@gmail.com>
2024-06-12 10:56:03 -0600

    Ejercicios JS actual

--- EjerciciosJS/app.js
+++ EjerciciosJS/app.js
@@ -1,4 +1,4 @@
-//EJERCICIOS DE ARRAYS
+/*EJERCICIOS SOBRE ARRAYS*/
 
 
 //EJERCICIO 1
@@ -18,7 +18,6 @@ const daysWork = []
 
 
 //EJERCICIO 3
-//pendiente de revisar
 var numsAll = new Array(10);
 numsAll = [2,4,6,10,35,23,11,8,12,5];
 
@@ -42,10 +41,89 @@ console.log(`Suma de numeros impares:`,noPair);
 
 //EJERCICIO 5
 
+//Definiendo arreglos
+const asistentesHTML = ["Pedro", "Juan", "Ana"];
+const asistentesCSS = ["Luis", "Juan", "Ana", "Maria"];
+
+// Función para encontrar asistentes que asisten a ambos cursos: pasando como parametros las listas
+
+function encontrarAsistentesAmbosCursos(asistentesHTML, asistentesCSS) {
+  // Filtrar el array de asistentesHTML para encontrar aquellos que también están en el array de asistentesCSS:
+  const asistentesAmbos = asistentesHTML.filter(asistente => asistentesCSS.includes(asistente));
+
+  // Ordenar la lista de asistentes,usando el metodo sort
+  asistentesAmbos.sort();
+
+  // Devolver la lista de asistentes a ambos cursos:
+  return asistentesAmbos;
+}
+
+//mostrando la funcion en consola
+
+console.log(encontrarAsistentesAmbosCursos(asistentesHTML, asistentesCSS));
+
+
+
+//EJERCICIO 6
+
+//se crea lista de objetos
+let integrantes = [
+    {nombre:"Juan", edad:34},
+    {nombre:"Pepa", edad:40},
+    {nombre:"Ana", edad:12},
+    {nombre:"Luis", edad:14}
+]
+
+    let max = integrantes.reduce(function(max, val){
+          
+    return val.edad > max.edad? val:max}, integrantes[0]);
+    let min = integrantes.reduce(function(max, val){
+         
+    return val.edad < max.edad? val:max}, integrantes[0]);
+    console.log("Miembro mayor: "+max.nombre+" de "+max.edad+" años");
+    console.log("Miembro más joven: "+min.nombre+" de "+min.edad+" años");
+
+//EJERCICIO 7
+//se declara el array
+const colores = ["Rojo","Verde","Azul","Negro","Purpura","Magenta","Cafe","Blanco","Gris"]
+
+//se crea la funcion, pasando como parametro el array
+function arrayGuion(colores){
+    
+        //accedemos a la primera posicion del array
+        let cadena = colores[0];
+        //se recorre la cadena a partir de la pos 1
+        for(let i=1; i<colores.length; i++){
+            cadena += "-/-" + colores[i]; //concatenado de elementos del array con un "-", esto añade un guion antes de cada elemento del array a partir de la pos 1, ej "rojo(pos 0)-verde(pos1)"
+            
+            }
+            return cadena; //retornar cadena de elementos con "-"
+            
+            }//imprimiendo funcion en consola
+            console.log(arrayGuion(colores));
 
 
 
 
+ //EJERCICIO 8
 
+function sumaLista(arrayNum){
+
+    const suma = arrayNum[0];
+
+    for(let i=0; i<arrayNum.length; i++){
+        suma = arrayNum[0]+suma;
+    }
+    return suma;
+
+
+}
+
+
+//EJERCICIO 9
+
+
+
+/*EJERCICIOS SOBRE OBJETOS*/
 
 
